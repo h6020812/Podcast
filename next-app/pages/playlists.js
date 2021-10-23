c72e81a329c521cb2243bac1a64892b800777a42
helloKaiDi <h6020812@gmail.com>
2021-10-23 16:48:59 +0800

    get playlists from graphql

--- next-app/pages/playlists.js
+++ next-app/pages/playlists.js
@@ -2,12 +2,38 @@ import SignIn from "../components/SignIn";
 import Episode from "../components/Episode";
 import { useAuth } from "../lib/auth";
 import { gql, useQuery, useMutation } from "@apollo/client";
-import { Container, FormControl, Select, Button, Flex } from "@chakra-ui/react";
+import { Container, FormControl, Select, Button, Flex, VStack } from "@chakra-ui/react";
 import { useState } from "react";
+import { AddIcon } from "@chakra-ui/icons";
+
+const GET_PLAYLISTS = gql`
+{
+    playlists {
+        name
+        episodes {
+            title
+            id
+            audio
+            summary
+            image
+            pubDate
+            podcast {
+                title
+                image
+            }
+        }
+    }
+}
+`
 
 export default function Playlists() {
     const { isSignedIn } = useAuth()
     const [selectedPlaylist, setSelectedPlaylist] = useState('')
+    const { data } = useQuery(GET_PLAYLISTS)
+
+    const filteredPlaylist = data?.playlists?.filter((p) => {
+        return p.name === selectedPlaylist
+    })[0]
 
     return (
         <Container>
@@ -17,10 +43,26 @@ export default function Playlists() {
                     <FormControl id="palylists">
                         <Flex>
                             <Select placeholder="Select playlist" onChange={(e) => setSelectedPlaylist(e.target.value)}>
-                                <option value="Foobar">Foobar</option>
+                                {data?.playlists?.map( (p) => {
+                                    return (
+                                        <option key={p.name} value={p.value}>
+                                            {p.name}
+                                        </option>
+                                    )
+                                })}
                             </Select>
+                            <Button ml={4}>
+                                <AddIcon />
+                            </Button>
                         </Flex>
                     </FormControl>
+                    <VStack mt={4} spacing={4}>
+                        {filteredPlaylist?.episodes?.map((e) => {
+                            return (
+                                <Episode key={e.id} episode={e} playlists={data.playlists} />
+                            )
+                        })}
+                    </VStack>
                 </div>
             )}
         </Container>
