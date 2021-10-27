dfb2b0bff3a0ba6176c7000ce4efef6c3f275d46
helloKaiDi <h6020812@gmail.com>
2021-10-27 14:51:21 +0800

    search podcasts func

--- next-app/pages/podcasts.js
+++ next-app/pages/podcasts.js
@@ -1,24 +1,64 @@
-import { useQuery, gql } from "@apollo/client";
+import { useLazyQuery, gql } from "@apollo/client";
+import { useState } from "react";
+import { useAuth } from "../lib/auth";
+import SignIn from "../components/SignIn";
+import { 
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    Container,
+    Flex,
+    VStack
+ } from "@chakra-ui/react";
 
-const PodcastQuery = gql`
-{
-    podcasts {
-        title
+const PodcastSearchQuery = gql`
+    query podcastSearch($searchTerm: String!) {
+        podcastSearch(searchTerm: $searchTerm) {
+            itunesId
+            title
+            description
+            feedURL
+            artwork
+            categories
+        }
     }
-}
 `
 
 const Podcasts = () => {
-    const { data } = useQuery(PodcastQuery);
+    const [getPodcasts, {data}] = useLazyQuery(PodcastSearchQuery);
+    const { isSignedIn } = useAuth()
+    const [searchString, setSearchString] = useState()
+
     return (
-        <div>
-            <h1>Podcasts:</h1>
-            <ul>
-                {data?.podcasts.map((p) => {
-                    return <li key={p.title}>{p.title}</li>
-                })}
-            </ul>
-        </div>
+        <Container>
+            {!isSignedIn() && <SignIn />}
+            {isSignedIn() && (
+                <div>
+                    <FormControl id="podcastsearch">
+                        <FormLabel>Search podcasts</FormLabel>
+                        <Flex>
+                            <Input onChange={(e) => setSearchString(e.target.value)} />
+                            <Button
+                                ml={4}
+                                onClick={
+                                    () => {
+                                        getPodcasts({ variables: { searchTerm: searchString }})
+                                    }
+                                }
+                            >
+                                Search
+                            </Button>
+                        </Flex>
+                    </FormControl>
+                    <ul>
+                        {data?.podcastSearch.map((p) => {
+                            return <li key={p.title}>{p.title}</li>
+                        })}
+                    </ul>
+                </div>
+            )}
+        </Container>
     )
 }
 
